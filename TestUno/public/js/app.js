fc3e5aaa34989744fd34a5059d53628f9f59b0a4
GianmarcoMontanari <giammont@live.it>
2020-07-02 13:04:36 +0200

    Fix-registration

--- TestUno/public/js/app.js
+++ TestUno/public/js/app.js
@@ -49839,8 +49839,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\39331\Desktop\MyProjects\test\TestUno\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\39331\Desktop\MyProjects\test\TestUno\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\MAMP\htdocs\PROVE MIE\PROVAGRUPPO\test\TestUno\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\MAMP\htdocs\PROVE MIE\PROVAGRUPPO\test\TestUno\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
